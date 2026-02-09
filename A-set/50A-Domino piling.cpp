6e32b3cbbc4f621c57fb145b09d024fe73d73f47
Bhaskar Nath <137477778+BhaskarNath27@users.noreply.github.com>
2026-02-09 21:13:49 +0530

    Add solution for domino piling problem

---
+++ A-set/50A-Domino piling.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int M,N;
+    cin>>M>>N;
+    
+    int area=M*N;
+    int ans=area/2;
+    cout<<ans<<endl;
+    
+    
+    return 0;
+}
+//TC=O(1)
+//SC=O(1)
+

f2732fadda70c5554fba5b4e5da341f496eb9478
Bhaskar Nath <137477778+BhaskarNath27@users.noreply.github.com>
2026-02-09 13:29:40 +0530

    Create 282A-Bit++.cpp

---
+++ A-set/282A-Bit++.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin>>n;
+    
+    int x=0;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        if(s[1]=='-'){
+            x--;
+        }else{
+            x++;
+        }
+        
+    }
+    cout<<x<<endl;
+    
+    
+    return 0;
+}
+//TC=O(N)
+//SC=O(1)
